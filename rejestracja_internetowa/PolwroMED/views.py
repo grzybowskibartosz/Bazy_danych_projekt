d968529bec7f802f0e5f9c523c3fc7a10ae40a2c
Bartosz Grzybowski <bartoszgrzybo2001@gmail.com>
2023-11-15 18:55:35 +0100

    New branches: Bartek i pre_master

--- rejestracja_internetowa/PolwroMED/views.py
+++ rejestracja_internetowa/PolwroMED/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Post
 
+
+
 def get_posts(request):
     posts = Post.objects.all()
     data = {"posts": [{"title": post.title, "content": post.content} for post in posts]}
