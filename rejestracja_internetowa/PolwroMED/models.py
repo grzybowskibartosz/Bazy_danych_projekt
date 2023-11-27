25a3f03e7961cdadcec8a1b570408bc596811a30
Bartosz Grzybowski <bartoszgrzybo2001@gmail.com>
2023-11-27 20:50:58 +0100

    Registration system in progress

--- rejestracja_internetowa/PolwroMED/models.py
+++ rejestracja_internetowa/PolwroMED/models.py
@@ -26,10 +26,26 @@ class Lekarz(models.Model):
     def __str__(self):
         return f'{self.imie} {self.nazwisko} - {self.specjalizacja}'
 
+
+class Gabinet(models.Model):
+    id = models.AutoField(primary_key=True)
+    lekarz = models.ForeignKey(Lekarz, on_delete=models.CASCADE)
+    numer_gabinetu = models.CharField(max_length=10, unique=True)
+    specjalizacja = models.CharField(max_length=100)
+    opis_gabinetu = models.TextField(blank=True, null=True)
+    status_dostepnosci = models.BooleanField(default=True)
+
+    lekarze = models.ManyToManyField('Lekarz', related_name='gabinety_lekarzy')
+
+    def __str__(self):
+        return self.numer_gabinetu
+
+
 class Wizyta(models.Model):
     id = models.AutoField(primary_key=True)
     data_i_godzina = models.DateTimeField(default=timezone.now)
     lekarz = models.ForeignKey(Lekarz, on_delete=models.CASCADE)
+    gabinet = models.ForeignKey(Gabinet, on_delete=models.CASCADE)
     pacjent = models.ForeignKey(Pacjent, on_delete=models.CASCADE)
     opis = models.TextField()
     STATUS_CHOICES = [
@@ -73,23 +89,15 @@ class Wizyta(models.Model):
         if self.status == 'Odbyta' and not self.przepisane_leki:
             raise ValidationError('Pole "Przepisane leki" jest wymagane dla wizyty odbytej.')
 
+        # Ustawianie daty i godziny wizyty tylko w przyszłość
+        if self.data_i_godzina and self.data_i_godzina < timezone.now():
+            raise ValidationError("Wizyty można umawiać tylko na przyszłość.")
+
     def save(self, *args, **kwargs):
         self.full_clean()  # Wywołanie metody clean przed zapisem
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'Wizyta u {self.lekarz} przez {self.pacjent} dnia {self.data_i_godzina}'
+        return f'Wizyta u {self.lekarz} przez {self.pacjent} dnia {self.data_i_godzina} w gabinecie {self.gabinet}'
 
-class Gabinet(models.Model):
-    id = models.AutoField(primary_key=True)
-    lekarz = models.ForeignKey(Lekarz, on_delete=models.CASCADE)
-    numer_gabinetu = models.CharField(max_length=10, unique=True)
-    specjalizacja = models.CharField(max_length=100)
-    opis_gabinetu = models.TextField(blank=True, null=True)
-    status_dostepnosci = models.BooleanField(default=True)
-
-    lekarze = models.ManyToManyField('Lekarz', related_name='gabinety_lekarzy')
-
-    def __str__(self):
-        return self.numer_gabinetu
 
